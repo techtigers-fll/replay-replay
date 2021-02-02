1b345a1ffc10cd86c95f36c68f3a1df59d32dfeb
Amu Ponnapalli <amuponnapalli44@gmail.com>
2021-02-02 17:36:34 -0500

    Made basketball + boccia work with new linear gear

--- basketball.py
+++ basketball.py
@@ -1,7 +1,7 @@
 from robot import Pid, Robot, LineEdge, LineSensor
 
 def run(robot: Robot):
-    straight_line_follow_pid = Pid(1, 0, 0)
+    straight_line_follow_pid = Pid(1, 0, 5)
     sharp_line_follow_pid = Pid(5, 0, 0)
     turn_pid = Pid(10, 0, 5)
     slow_turn_pid = Pid(3, 0, 0)
@@ -12,10 +12,10 @@ def run(robot: Robot):
     robot.turn(turn_pid, 55)
     robot.drive(drive_pid, 400, 55, 725) 
     robot.turn(turn_pid, 0)
-    robot.follow_line(straight_line_follow_pid, 400, 1500, LineSensor.RIGHT, LineEdge.RIGHT)
+    robot.follow_line(straight_line_follow_pid, 400, 1600, LineSensor.RIGHT, LineEdge.RIGHT)
     robot.move_linear(-800, 6.15)
-    robot.move_linear(800, 4.9)
-    robot.move_linear(-800, 4.9)
+    robot.move_linear(800, 5.1)
+    robot.move_linear(-800, 5.1)
     robot.move_linear(800, 0.5)
     robot.move_linear(800, 5.65, False)
     robot.drive(drive_pid, -400, 0, 600)
--- sandbox.py
+++ sandbox.py
@@ -18,11 +18,16 @@ def run(robot: Robot):
     robot.reset_sensors()
 
     # robot.drive(drive_pid, 400, 0, 3500)
-    robot.drive(sharp_drive_pid, -400, 30, 600)
-    robot.turn(turn_pid, -32)
-    robot.move_linear(-800, 2, False)
-    robot.drive(drive_pid, 200, -32, 1675)
-    robot.move_linear(800, 1.6)
-    robot.drive(drive_pid, -200, -32, 1000)
 
-    robot.move_linear(800, 0.3)
+    # robot.drive(sharp_drive_pid, -400, 30, 600)
+    # robot.turn(turn_pid, -32)
+    # robot.move_linear(-800, 1, False)
+    # robot.drive(drive_pid, 200, -32, 1700)
+
+    robot.move_linear(-400, 1.3)
+    robot.drive(drive_pid, 150, 0, 1000)
+    robot.move_linear(400, 1.05, False)
+    wait(200)
+    robot.drive(drive_pid, -150, 0, 1500)
+
+    # robot.move_linear(800, 0.3)
