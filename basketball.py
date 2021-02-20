9438795361a8abdc0b067d33edb51e69c73469f0
Amu Ponnapalli <amuponnapalli44@gmail.com>
2021-02-20 09:37:20 -0500

    Changing linear gear :(((((((((

--- basketball.py
+++ basketball.py
@@ -13,6 +13,8 @@ def run(robot: Robot):
     drive_pid = Pid(1, 0, 0)
     robot.reset_sensors()
 
+
+
     robot.drive(drive_pid, 400, 0, 450) 
     robot.turn(turn_pid, 55)
     robot.drive(drive_pid, 400, 55, 900) 
@@ -21,7 +23,7 @@ def run(robot: Robot):
     robot.turn(turn_pid, 0)
     robot.drive(drive_pid, 400, 0, 1600)
 
-    robot.move_linear(-800, 6.7)
+    robot.move_linear(-800, 6.55)
     robot.move_linear(800, 6)
     robot.move_linear(-800, 6)
     robot.move_linear(800, 0.5)
@@ -36,33 +38,32 @@ def run(robot: Robot):
     robot.align(200, LineSensor.LEFT, LineSensor.CENTER)
     robot.turn(turn_pid, 60)
     robot.drive(drive_pid, 100, 60, 1200)
-    robot.drive(drive_pid, -100, 60, 300)
+    robot.drive(drive_pid, -100, 60, 100)
     robot.move_linear(-800, 2.5)
     robot.move_linear(800,0.5)
-    robot.move_linear(800, 3.75, False)
+    # robot.move_linear(800, 3.5, False)
     robot.stop_on_black(drive_pid, -100, 60, LineSensor.CENTER)
     robot.turn(turn_pid, 0)
-    robot.drive(drive_pid, 200, 0, 800)
+    robot.drive(drive_pid, 200, 0, 700)
     robot.move_dropper(100, 45)
     
     wait(500)
-    robot.drive(drive_pid, -200, -10, 1700)
-    robot.align(100, LineSensor.LEFT, LineSensor.CENTER)
-    robot.drive(drive_pid, -200, -10, 1600)
-    robot.stop_on_black(drive_pid, 200, -10, LineSensor.CENTER)
+    robot.drive(drive_pid, -100, 0, 300)
+    wait(300)
+    robot.stop_on_black(drive_pid, -200, 0, LineSensor.CENTER) 
+    robot.move_linear(800, 2, False)
+    robot.drive(drive_pid, 200, 0, 300)
+    robot.turn(turn_pid, -48)
 
-    robot.drive(drive_pid, 200, 0, 400)
-    robot.turn(turn_pid, -52)
-    robot.drive(drive_pid, 200, -52, 1600) 
+    robot.drive(drive_pid, 200, -48, 1700)
     robot.turn(slow_turn_pid, -135)
-    robot.drive(drive_pid, 200, -135, 1600)
-    robot.turn(slow_turn_pid, -110)
-    robot.drive(drive_pid, 200, -110, 1100)
-    robot.drive(drive_pid, -200,-110, 800)
-    robot.turn(turn_pid, -60)
-    robot.drive(drive_pid, 400, -60, 800)
-    robot.drive(drive_pid, 200, -60, 1100)
+    robot.drive(drive_pid, 200, -135, 1700)
+    robot.turn(slow_turn_pid, -65)
+    robot.drive(drive_pid, 400, -65, 800)
+    robot.drive(drive_pid, 200, -65, 1100)
     robot.turn(slow_turn_pid, -90)
-    robot.drive(drive_pid, 200, -90, 1500)
+    robot.drive(drive_pid, 200, -90, 1800)
+    
+    
 
      
