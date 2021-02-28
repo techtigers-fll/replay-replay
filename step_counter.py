da8b2a6e547bba8b96de60237ab44f6dddbc61c4
Amu Ponnapalli <amuponnapalli44@gmail.com>
2021-02-27 21:38:49 -0500

    Speeding up step counter run

--- step_counter.py
+++ step_counter.py
@@ -20,7 +20,8 @@ def run(robot: Robot):
 
     # Going to step counter
     robot.linear_attachment_motor.run_until_stalled(200, Stop.BRAKE, 20)
-    robot.drive(drive_pid, 400, 3, 3700)
+    robot.drive(drive_pid, 400, 3, 3000)
+    robot.drive(drive_pid, 200, 3, 500)
 
     # Backing out and squaring on wall
     robot.stop_on_white(sharp_drive_pid, -100, 10, LineSensor.CENTER)
@@ -31,12 +32,12 @@ def run(robot: Robot):
     robot.reset_sensors(-90)
 
     # Doing pullup bar
-    robot.drive(drive_pid, 800, -90, 700)
+    robot.drive(drive_pid, 400, -90, 700)
     robot.follow_line(straight_line_follow_pid, 200, 1000, LineSensor.RIGHT, LineEdge.RIGHT)
-    robot.drive(sharp_drive_pid, 800, -90, 350)
+    robot.drive(sharp_drive_pid, 400, -90, 350)
     robot.stop_on_black(drive_pid, 200, -90, LineSensor.LEFT)
     robot.drive(drive_pid, 200, -90, 250)
-    robot.drive(drive_pid, -800, -90, 1400)
+    robot.drive(drive_pid, -400, -90, 1400)
     robot.stop_on_black(drive_pid, -200, -90, LineSensor.RIGHT)
 
     # Turning to blue tire
@@ -44,12 +45,12 @@ def run(robot: Robot):
     robot.turn(slow_turn_pid, -30)
 
     # Doing blue tire
-    robot.move_linear(-800, 1.3, False)
+    robot.move_linear(-800, 1.4, False)
     robot.drive(drive_pid, 200, -30, 800)
     robot.drive(drive_pid, 150, -35, 900)
     robot.drive(drive_pid, -150, -35, 370)
 
-    robot.move_linear(200, 0.68)
+    robot.move_linear(200, 0.74)
     robot.drive(drive_pid, -150, -35, 200)
     robot.move_linear(400, 0.3, False)
     robot.drive(drive_pid, -150, -35, 1300)
@@ -61,19 +62,18 @@ def run(robot: Robot):
     robot.drive(drive_pid, 100, -35, 300)
     robot.turn(turn_pid, 0)
     robot.follow_line(straight_line_follow_pid, 200, 2000, LineSensor.RIGHT, LineEdge.RIGHT)
-    robot.turn(turn_pid, 0)
-    robot.align(100, LineSensor.LEFT, LineSensor.CENTER)
+    robot.stop_on_black(sharp_drive_pid, 200, 0, LineSensor.LEFT)
     robot.turn(turn_pid, 2)
 
     # Treadmill mission
     robot.drive(drive_pid, 300, 2, 1000)
-    robot.right_motor.run_time(800, 3500, Stop.BRAKE)
+    robot.right_motor.run_time(800, 3000, Stop.BRAKE)
 
     # Backing into wall
-    robot.drive(drive_pid,-200, 0, 500)
+    robot.drive(drive_pid,-200, 0, 1000)
     robot.stop_on_black(drive_pid, -50, 0, LineSensor.LEFT)
     robot.turn(turn_pid, -90) 
-    robot.drive(drive_pid, -200, -90, 1200)
+    robot.drive(drive_pid, -400, -90, 600)
     robot.reset_sensors(-90)
 
     # Doing row machine
@@ -90,21 +90,23 @@ def run(robot: Robot):
     # Doing weight machine
     robot.move_linear(-600, 5.8, False)
     robot.turn(slow_turn_pid, -90)
-    robot.drive(drive_pid, 200, -90, 500)
-    robot.align(200, LineSensor.LEFT, LineSensor.CENTER)
-    robot.drive(drive_pid, 400, -90, 700)
+    robot.drive(drive_pid, 400, -90, 1250)
+    robot.drive(drive_pid, 200, -90, 1200)
+    robot.reset_sensors(-90)
+    robot.drive(drive_pid, -200, -90, 500)
     robot.turn(slow_turn_pid, 0)
     robot.drive(drive_pid, 200, 0, 150)
     robot.linear_attachment_motor.run_until_stalled(-200, Stop.BRAKE, 20)
-    robot.move_linear(600, 5)
+    robot.move_linear(800, 5)
     robot.drive(drive_pid, -400, 0, 300)
     robot.drive(drive_pid, 400, 0, 800)
     
     # Going to black tire
     wait(100)
-    robot.drive(sharp_drive_pid, -400, 0, 375) # 300 when set up from weight machine
+    # robot.move_linear(-800, 0.7)
+    robot.drive(sharp_drive_pid, -400, 0, 375) # 300 when set up from weight machine, otherwise 375
     robot.turn(slow_turn_pid, 90)
-    robot.drive(sharp_drive_pid, 200, 90, 600)
+    robot.drive(sharp_drive_pid, 400, 90, 400)
     robot.follow_line(straight_line_follow_pid, 100, 700, LineSensor.RIGHT, LineEdge.LEFT)
     robot.stop_on_white(drive_pid, 150, 90, LineSensor.LEFT)
     robot.move_linear(-600, 0.5, False)
@@ -116,17 +118,17 @@ def run(robot: Robot):
     robot.drive(drive_pid, -200, 125, 300)
     robot.move_linear(400, 0.85)
     robot.drive(drive_pid, -400, 125, 600)
-    robot.turn(slow_turn_pid, 110)
+    robot.turn(turn_pid, 110)
     robot.drive(drive_pid, 200, 110, 1500)
     robot.move_dropper(100, 45)
 
     # Going to dance
     robot.drive(drive_pid, -200, 110, 600)
     robot.stop_on_white(drive_pid, -150, 110, LineSensor.LEFT)
-    robot.turn(slow_turn_pid, 180)
-    robot.follow_line(straight_line_follow_pid, 200, 2000, LineSensor.LEFT, LineEdge.LEFT)
-    robot.stop_on_black(sharp_drive_pid, 150, 180, LineSensor.RIGHT)
+    robot.drive(drive_pid, 200, 110, 300)
+    robot.turn(turn_pid, 180)
+    robot.drive(drive_pid, 400, 180, 1000)
 
-    robot.turn(slow_turn_pid, -130)
-    robot.drive(drive_pid, 400, -130, 800)
+    robot.turn(turn_pid, -130)
+    robot.drive(drive_pid, 400, -130, 1200)
 
